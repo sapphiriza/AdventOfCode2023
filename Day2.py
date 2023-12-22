26f6c2d103ed28199ecdf085c88a86c0fe5c73af
Sapphira Riza <sapphiriza@gmail.com>
2023-12-22 14:29:12 -0800

    Day 2 part 2!

--- Day2.py
+++ Day2.py
@@ -6,8 +6,10 @@ numCubesPerColor = {
 
 if __name__ == "__main__":
 	file = open("input/cubeGames.txt")
+	# file = open("input/easyCubes.txt")
 	lines = file.readlines()
 	validGameSum = 0
+	gamePowerSum = 0
 	for line in lines:
 		# Get the game ID
 		strings = line.split(':')
@@ -15,15 +17,24 @@ if __name__ == "__main__":
 		# Split the game
 		games = strings[1].split(';')
 		possible = True
+		minCubesPerColor = {
+			"red": 0,
+			"green": 0,
+			"blue": 0
+		}
+
 		for game in games:
 			pulls = game.split(',')
 			for pull in pulls:
 				pull = pull.strip().split(' ')
-				if int(pull[0]) > numCubesPerColor[pull[1]]:
+				numCubes = int(pull[0])
+				if numCubes > minCubesPerColor[pull[1]]:
+					minCubesPerColor[pull[1]] = numCubes
+				if numCubes > numCubesPerColor[pull[1]]:
 					possible = False
-					break
-			if not possible:
-				break
 		if possible:
 			validGameSum += gameID
+		gamePowerSum += minCubesPerColor["red"] * minCubesPerColor["green"] * minCubesPerColor["blue"]
+	
 	print("Some of those games were possible! Now I'm going to say a number! " + str(validGameSum))
+	print("Wow, those were some powerful games! Uhhh " + str(gamePowerSum))
